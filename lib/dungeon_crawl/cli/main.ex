e0086934eb971080e112ac2936ce337136967dc4
Riku Kestilä <rikukestila@gmail.com>
2023-03-31 16:15:52 +0300

    Hero selection

--- lib/dungeon_crawl/cli/main.ex
+++ lib/dungeon_crawl/cli/main.ex
@@ -3,11 +3,17 @@ defmodule DungeonCrawl.CLI.Main do
 
   def start_game do
     welcome_message()
+    Shell.prompt("Press enter to continue")
+    hero_choice()
   end
 
   defp welcome_message do
     Shell.info("== Welcome to Dungeon Crawl! ==")
     Shell.info("You awake in a dungeonfull of monsters.")
     Shell.info("You must  survive and find the exit to escape.")
+  end
+
+  defp hero_choice do
+    DungeonCrawl.CLI.HeroChoice.start()
   end
 end
